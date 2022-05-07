879180c4647642ff85731e79354e29a66f136ddf
Thalyson <thalysonbarrospereira2019@gmail.com>
2022-05-07 15:50:49 -0300

    Adicionando composer ao projeto

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'c642a2aebe84e4af52cf8768e0fa760f45764107',
+        'name' => 'storewhats/app',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'storewhats/app' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'c642a2aebe84e4af52cf8768e0fa760f45764107',
+            'dev_requirement' => false,
+        ),
+    ),
+);
